33df10c04297de4149e7a74138c19d7ed4c6ded9
Juri Berlanda <juriberlanda@hotmail.com>
2016-11-29 20:51:32 +0100

    [Unity] Add first version of Collider based API.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

---
+++ Unity/StinkomatApi/StinkomatController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class StinkomatController : MonoBehaviour {
+	private const int numScents = 6;
+
+	public SocketIOComponent client;
+	[Range(0, numScents - 1)]
+	public int[] scents;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	new void OnTriggerEnter() {
+		Debug.Log("Trigger enter");
+		foreach (int scent in scents)
+			client.Emit("activate", new JSONObject(scent));
+	}
+
+	new void OnTriggerExit() {
+		Debug.Log("Trigger exit");
+		foreach (int scent in scents)
+			client.Emit("deactivate", new JSONObject(scent));
+	}
+}
