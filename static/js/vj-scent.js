7854afadbaf4c1e1361aaad54bdf4925494648d9
Juri Berlanda <juriberlanda@hotmail.com>
2017-01-13 19:32:44 +0100

    [Static] First draft of MaterializeCss based UI.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- static/js/vj-scent.js
+++ static/js/vj-scent.js
@@ -5,14 +5,24 @@ socket.on('connect', function(msg) {
     console.log('[INFO] Socket connected.');
 });
 
-function activate() {
-    scent = $('#scent').val();
+function activate(scent) {
     console.log('Activating scent: ' + scent);
     socket.emit('activate', scent);
 }
 
-function deactivate() {
-    scent = $('#scent').val();
+function deactivate(scent) {
     console.log('Deactivating scent: ' + scent);
     socket.emit('deactivate', scent);
 }
+
+function handleClick(cb) {
+    if (cb.checked)
+        activate(cb.id);
+    else
+        deactivate(cb.id);
+}
+
+var scentModel = {
+    availableScents: [0, 1, 2, 3, 4, 5]
+};
+ko.applyBindings(scentModel);
