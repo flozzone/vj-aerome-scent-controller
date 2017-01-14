7621fcd9fe8236133253190e31f223fc6386ec8e
Juri Berlanda <juriberlanda@hotmail.com>
2017-01-14 20:55:36 +0100

    [WebUI] Implement state fetching on load.
    TODO: Find out what actual RasPi returns for the states.
    
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- static/js/vj-scent.js
+++ static/js/vj-scent.js
@@ -1,3 +1,6 @@
+SCENT_STATUS_URL = "/status";
+SCENT_ON_VALUE = "HIGH";
+
 console.log("Start");
 var socket = io.connect('http://' + document.domain + ':' + location.port + '/scent');
 
@@ -22,7 +25,23 @@ function handleClick(cb) {
         deactivate(cb.id);
 }
 
+function applyState(data) {
+    $.each(data, function(key, val) {
+        var cb =$('#scentCheckboxes').find('#' + key);
+        cb.prop('checked', val == SCENT_ON_VALUE);
+        cb.prop('disabled', false);
+    });
+}
+
+function fetchState() {
+    $.ajax({
+        url: SCENT_STATUS_URL,
+        success: applyState
+    });
+}
+
 var scentModel = {
     availableScents: [0, 1, 2, 3, 4, 5]
 };
 ko.applyBindings(scentModel);
+fetchState();
